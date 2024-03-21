db6f7f836afbbeef34bc9f11fffb8dd9eff9b39d
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2024-03-21 11:35:52 -0300

    Add project files.

---
+++ Diamante_X.ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Diamante_X.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao, espaço, asterisco = 1;
+            
+            do
+            {
+                Console.Clear(); 
+                Console.WriteLine("DIAMANTE EM X");
+                Console.WriteLine();
+                Console.Write("Digite um número impar, (0 para sair):");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                if (opcao % 2 != 0)
+                {
+                    espaço = opcao / 2;
+
+                    #region //Parte superior
+                    for (int i = 0; i < espaço; i++)
+                    {
+                        for (int j = i; j < espaço; j++)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for (int h = 0; h < asterisco; h++)
+                        {
+                            Console.Write("X");
+                        }
+
+                        asterisco += 2;
+
+
+                        Console.WriteLine("");
+
+                    }
+                    #endregion
+
+
+                    #region//meio do diamante
+                    for (int i = 0; i < opcao; i++)
+                    {
+                        Console.Write("X");
+                    }
+                    Console.WriteLine();
+                    #endregion
+
+
+                    #region //parte de inferior
+                    asterisco = opcao - 1;
+
+                    for (int i = espaço; i < asterisco; i--)
+                    {
+                        for (int j = i; j <= espaço; j++)
+                        {
+                            Console.Write(" ");
+                        }
+                        for (int h = 1; h < asterisco; h++)
+                        {
+                            Console.Write("x");
+                        }
+
+                        asterisco -= 2;
+
+                        Console.WriteLine("");
+                    }
+                    #endregion
+                }
+                else if (opcao== 0)
+                    break;
+                else
+                    Console.WriteLine("Opção invalida. Tente novamente");
+
+                Console.ReadLine();
+
+            }while (opcao  != 0);
+        }
+
+        
+    }
+}
